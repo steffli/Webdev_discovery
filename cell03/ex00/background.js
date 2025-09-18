755f6f344d824ad2c5b38960fdcfe1aa34f7bfba
Stephan Liu <stliu@1-D-3.42heilbronn.de>
2025-09-18 18:05:21 +0200

    added cell03

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -2,12 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('colorBtn');
     
     button.addEventListener('click', function() {
-        // Generate random RGB values
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        
-        // Apply the random color to the button
         this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     });
 });
